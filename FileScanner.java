3769f4120e9e03a58d0862e3501515078aebea08
RavillaMonika <monikaravilla57@gmail.com>
2026-06-22 16:44:10 +0530

    Delete FileScanner.java

--- FileScanner.java
+++
@@ -1,9 +0,0 @@
-public class FileScanner {
-
-    public static void scan(String code) {
-
-        if(code.contains("password")) {
-            System.out.println("WARNING: Hardcoded Password Found");
-        }
-    }
-}
